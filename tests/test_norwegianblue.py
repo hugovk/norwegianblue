018077a4d4663c5657f3455ca7851d9e13ea723c
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-02-05 18:38:06 +0200

    If product not found, offer a suggestion

--- tests/test_norwegianblue.py
+++ tests/test_norwegianblue.py
@@ -372,12 +372,20 @@ class TestNorwegianBlue:
     @respx.mock
     def test_404(self) -> None:
         # Arrange
-        mocked_url = "https://endoflife.date/api/this-product-not-found.json"
+        mocked_url = "https://endoflife.date/api/androd.json"
+        respx.get(mocked_url).respond(status_code=404)
+
+        mocked_url = "https://endoflife.date/api/all.json"
+        mocked_response = SAMPLE_RESPONSE_ALL_JSON
+        respx.get(mocked_url).respond(content=mocked_response)
 
         # Act / Assert
-        respx.get(mocked_url).respond(status_code=404)
-        with pytest.raises(ValueError, match=norwegianblue.ERROR_404_TEXT):
-            norwegianblue.norwegianblue(product="this-product-not-found")
+        with pytest.raises(
+            ValueError,
+            match=r"Product 'androd' not found, run 'eol all' for list\. "
+            r"Did you mean: 'android'?",
+        ):
+            norwegianblue.norwegianblue(product="androd")
 
     def test_norwegianblue_norwegianblue(self) -> None:
         # Act
